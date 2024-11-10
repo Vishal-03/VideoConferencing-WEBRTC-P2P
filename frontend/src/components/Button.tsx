6efe425348f612de5103b781a5717ea8ded8d849
Vishal Choudhary <vishalchoudhary@Vishals-MacBook-Air.local>
2024-11-11 05:01:25 +0530

    error in button.tsx removed

--- frontend/src/components/Button.tsx
+++ frontend/src/components/Button.tsx
@@ -1,10 +1,7 @@
-import { useState } from "react";
 
 export const Button = () => {
-    const [joined, setJoined] = useState(false);
     return <div>
         <button className="px-12 py-4 rounded-full bg-[#1ED760] font-bold text-white tracking-widest uppercase transform hover:scale-105 hover:bg-[#21e065] transition-colors duration-200" onClick={() => {
-                setJoined(true);
             }}>Join</button>
     </div>
 }
